8c11434dec891b177e1f0d2390137826e5cb05b9
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-02-09 16:10:10 -0300

    kaminari working default

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,7 +3,7 @@ class TweetsController < ApplicationController
 
   # GET /tweets or /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.page(params[:page])
   end
 
   # GET /tweets/1 or /tweets/1.json
