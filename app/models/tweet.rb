8193648a98c44621c6ba41e249de4b46a66354e6
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-02-09 17:02:02 -0300

    methods to bring user name, and user photo created

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,4 +3,12 @@ class Tweet < ApplicationRecord
     has_many :likes
     has_many :retweets, class_name: "Tweet", foreign_key: "Tweet_id"
     belongs_to :tweet, class_name: "Tweet", optional: true
+
+    def get_user_name
+        self.user.name
+    end
+
+    def get_user_photo
+        self.user.photo_url
+    end
 end
