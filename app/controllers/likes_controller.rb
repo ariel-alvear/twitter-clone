8af181d84994c238c75bd929baa5473df7a82d95
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-02-11 09:53:09 -0300

    users can't like more than once

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,13 @@
 class LikesController < ApplicationController
     before_action :find_tweet
+
     def create
         if user_signed_in?
-            @tweet.likes.create(user_id: current_user.id)
+            if already_liked?
+                flash[:notice] = "You can't like more than once"
+            else
+                @tweet.likes.create(user_id: current_user.id)
+            end
             redirect_to root_path
         else
             redirect_to new_user_session_path
@@ -12,5 +17,9 @@ class LikesController < ApplicationController
     private
     def find_tweet
         @tweet = Tweet.find(params[:tweet_id])
+    end
+
+    def already_liked?
+        Like.where(user_id: current_user.id, tweet_id: params[:tweet_id]).exists?
     end
 end
