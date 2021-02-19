c1578e046f8d5b8d7f482186e744e6c7c62dbd45
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-02-19 10:51:34 -0300

    Seed hito 1 created

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do
-    Tweet.create(content: "Seed Tweet", user_id: 6)
+100.times do
+    Tweet.create(content: "Seed Tweet", user_id: 3)
 end
