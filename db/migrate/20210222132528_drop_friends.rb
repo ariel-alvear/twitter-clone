02f438e5503db05cb3faff836982f1f26d289206
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-02-24 09:06:18 -0300

    db migrations adapted to upload in heroku

--- db/migrate/20210222132528_drop_friends.rb
+++
@@ -1,5 +0,0 @@
-class DropFriends < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :friends
-  end
-end
--- db/migrate/20210222181959_drop_friends_again.rb
+++
@@ -1,5 +0,0 @@
-class DropFriendsAgain < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :friends
-  end
-end
